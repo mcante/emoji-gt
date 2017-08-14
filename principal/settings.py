c7af201fe3020ecacbd9d727e3d8d690dd638e36
mrcante <melvincante@gmail.com>
2017-08-14 16:27:26 -0600

    Ajustes de responsive en calendario

--- principal/settings.py
+++ principal/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'ecmbfmscwyfcra0zgu%5rqotdz+kd#53fj@3jv8ct=*xw_b%^u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
